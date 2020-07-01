655c75382fd16b0c1c19185f30c169e42ca77b4d
tsgcpp <tsgcpp.public8x@gmail.com>
2020-07-01 17:51:23 +0900

    Add CubemapFileGenerator

---
+++ Assets/Editor/CubemapFileGenerator/Cubemap2HorizontalPngConverter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CubemapFileGenerator
+{
+    public class Cubemap2HorizontalPngConverter : ICubemapConverter
+    {
+        public byte[] Convert(Cubemap cubemap)
+        {
+            int width = cubemap.width;
+            int height = cubemap.height;
+
+            Texture2D texture = new Texture2D(
+                width * 6, height,
+                TextureFormat.RGB24, false);
+
+            var faces = new CubemapFace[] {
+            CubemapFace.PositiveX,
+            CubemapFace.NegativeX,
+            CubemapFace.PositiveY,
+            CubemapFace.NegativeY,
+            CubemapFace.PositiveZ,
+            CubemapFace.NegativeZ,
+        };
+
+            int x = 0;
+            foreach (var face in faces) {
+                texture.SetPixels(x, 0, width, height, cubemap, face);
+                x += width;
+            }
+            texture.Apply();
+
+            return ImageConversion.EncodeToPNG(texture);
+        }
+    }
+}
---
+++ Assets/Editor/CubemapFileGenerator/RenderCubemapWizard.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace CubemapFileGenerator
+{
+    public class RenderCubemapWizard : ScriptableWizard
+    {
+        public int width = 256;
+        public FileType fileType = FileType.VerticalPng;
+        public Transform renderFromPosition;
+
+        // レンダリングカメラの位置を表すGameObjectの名前
+        const string DefaultTransformPositionName = "CubemapRenderingPoint";
+
+        [MenuItem("CubemapFileGenerator/Generate cubemap file")]
+        static void RenderCubemap()
+        {
+            ScriptableWizard.DisplayWizard<RenderCubemapWizard>(
+                "Generate cubemap file", "Generate!");
+        }
+
+        void OnWizardUpdate()
+        {
+            helpString = "Select transform to render from and cubemap to render into";
+
+            if (renderFromPosition == null) {
+                var renderingPointGO = GameObject.Find(DefaultTransformPositionName);
+                renderFromPosition = renderingPointGO?.transform;
+            }
+
+            isValid = (renderFromPosition != null);//  && (renderCubemap != null);
+        }
+
+        void OnWizardCreate()
+        {
+            string path = EditorUtility.SaveFilePanel(
+                    title: "Save png",
+                    directory: "",
+                    defaultName: "GeneratedCubemap.png",
+                    extension: "png");
+
+            if (path == null || path == "") {
+                Debug.LogError(string.Format("Invalid path: \"{0}\"", path));
+                return;
+            }
+
+            Cubemap cubemap = new Cubemap(width, TextureFormat.RGB24, false);
+
+            RenderToCubemap(cubemap);
+            EncodeToPng(cubemap, path);
+
+            AssetDatabase.Refresh();
+        }
+
+        private void RenderToCubemap(Cubemap cubemap)
+        {
+            GameObject go = new GameObject("CubemapCamera");
+
+            go.AddComponent<Camera>();
+            go.transform.position = renderFromPosition.position;
+            go.transform.rotation = Quaternion.identity;
+            go.GetComponent<Camera>().RenderToCubemap(cubemap);
+
+            DestroyImmediate(go);
+        }
+
+        void EncodeToPng(Cubemap cubemap, string path)
+        {
+            var converter = GetConverter(fileType);
+            byte[] pngBytes = converter.Convert(cubemap);
+            File.WriteAllBytes(path, pngBytes);
+        }
+
+        private static ICubemapConverter GetConverter(FileType fileType)
+        {
+            switch (fileType) {
+                case FileType.HorizontalPng:
+                    return new Cubemap2HorizontalPngConverter();
+                default:
+                    return new Cubemap2VerticalPngConverter();
+            }
+        }
+
+        public enum FileType
+        {
+            VerticalPng,
+            HorizontalPng,
+        }
+    }
+}
